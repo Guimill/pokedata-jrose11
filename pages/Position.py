dd6941ad58976756f04ab4812b8a2a15b950a89a
Guimill <86968380+Guimill@users.noreply.github.com>
2024-02-29 19:16:23 +0000

    position finish

--- pages/Position.py
+++ pages/Position.py
@@ -50,46 +50,124 @@ Stats = st.radio("Choose the Stats you'd like to display :",
 
 data = []
 
+pokepos_sorted = pokedata.sort_values(by=Stats)
+legend_labels_pos = set()
 
-pokename_sorted = pokedata.sort_values(by=locals()[Stats])
-legend_labels = set()
+X_pos = sm.add_constant(pokepos_sorted['POSITION'])  
+model_pos = sm.OLS(pokepos_sorted[Stats], X_pos).fit()
+const_pos, slope_pos = model_pos.params
 
-fig = go.Figure()
+spearman_corr = pokepos_sorted[['POSITION', Stats]].corr(method='spearman').iloc[0, 1]
+
+fig_pos = go.Figure()
 
 # Add scatter trace for each row
-for index, row in pokename_sorted.iterrows():
-    value = row[locals()[Stats]]
+for index, row in pokepos_sorted.iterrows():
+    value = row[Stats]
     dtypes = row['DTYPES']
     colordt_left = dt_type_pal_new_double.get(dtypes)[0]
     colordt_right = dt_type_pal_new_double.get(dtypes)[1]
-    if dtypes not in legend_labels:
-        scat = go.Scatter(x=[row['POSITION']], y=[value], mode='markers', marker=dict(color=colordt_left, symbol='circle', size=10, line=dict(width=3, color=colordt_right)), name=dtypes, text=pokemon_name)
-        fig.add_trace(scat)
-        legend_labels.add(dtypes)
+    if dtypes not in legend_labels_pos:
+        scat = go.Scatter(x=[row['POSITION']], y=[value], mode='markers', marker=dict(color=colordt_left, symbol='circle', size=10, line=dict(width=3, color=colordt_right)), name=dtypes)
+        fig_pos.add_trace(scat)
+        legend_labels_pos.add(dtypes)
     else:
-        scat = go.Scatter(x=[row['POSITION']], y=[value], mode='markers', marker=dict(color=colordt_left, symbol='circle', size=10, line=dict(width=3, color=colordt_right)), showlegend=False, text=pokemon_name)
-        fig.add_trace(scat)
+        scat = go.Scatter(x=[row['POSITION']], y=[value], mode='markers', marker=dict(color=colordt_left, symbol='circle', size=10, line=dict(width=3, color=colordt_right)), showlegend=False)
+        fig_pos.add_trace(scat)
 
+
+regression_line = const_pos + slope_pos * pokepos_sorted['POSITION']
+fig_pos.add_trace(go.Scatter(x=pokepos_sorted['POSITION'], y=regression_line, mode='lines', name='Linear Regression', line=dict(color='red')))
 
 # Update layout
-fig.update_layout(
+fig_pos.update_layout(
     xaxis=dict(title='Position',
         tickfont=dict(size=20),
         titlefont=dict(size=25)),
-    yaxis=dict(title='Length of Pokemon\'s names',
+    yaxis=dict(title=Stats,
         tickfont=dict(size=20),
         titlefont=dict(size=25)),
     legend=dict(title='Types'),
     width=1280,  # Adjust the width of the plot
     height=720,  # Adjust the height of the plot
     title={
-        'text': "Scatter Plot of Pokemon Length vs. Position",
+        'text': "Scatter Plot of Pokemon {Stats} vs. Position",
         'x': 0.5,  # Set title's x position to center
         'xanchor': 'center',  # Anchor title to the center
         'font': {'size': 30}
     },
-    margin=dict(t=100)
+    margin=dict(t=100),
+    annotations=[dict(
+        text=f"Spearman Correlation: {spearman_corr * -100:.2f}%",
+        x=0.95, y=0.95,
+        xref="paper", yref="paper",
+        showarrow=False,
+        font=dict(color="black", size=12),
+        bgcolor="#19c37d", opacity=0.8
+    )],
 )
 
 # Show plot
-st.plotly_chart(fig)
+st.plotly_chart(fig_pos)
+
+data = []
+
+poketiers_sorted = pokedata.sort_values(by=Stats)
+legend_labels_tiers = set()
+
+X_Tiers = sm.add_constant(poketiers_sorted['TIERS'])  
+model_tiers = sm.OLS(poketiers_sorted[Stats], X_Tiers).fit()
+const_tiers, slope_tiers = model_tiers.params
+
+spearman_corr = poketiers_sorted[['TIERS', Stats]].corr(method='spearman').iloc[0, 1]
+
+fig_tiers = go.Figure()
+
+# Add scatter trace for each row
+for index, row in poketiers_sorted.iterrows():
+    value = row[Stats]
+    dtypes = row['DTYPES']
+    colordt_left = dt_type_pal_new_double.get(dtypes)[0]
+    colordt_right = dt_type_pal_new_double.get(dtypes)[1]
+    if dtypes not in legend_labels_tiers:
+        scat = go.Scatter(x=[row['TIERS']], y=[value], mode='markers', marker=dict(color=colordt_left, symbol='circle', size=10, line=dict(width=3, color=colordt_right)), name=dtypes)
+        fig_tiers.add_trace(scat)
+        legend_labels_tiers.add(dtypes)
+    else:
+        scat = go.Scatter(x=[row['TIERS']], y=[value], mode='markers', marker=dict(color=colordt_left, symbol='circle', size=10, line=dict(width=3, color=colordt_right)), showlegend=False)
+        fig_tiers.add_trace(scat)
+
+
+regression_line = const_tiers + slope_tiers * poketiers_sorted['TIERS']
+fig_tiers.add_trace(go.Scatter(x=poketiers_sorted['TIERS'], y=regression_line, mode='lines', name='Linear Regression', line=dict(color='red')))
+
+# Update layout
+fig_tiers.update_layout(
+    xaxis=dict(title="Tiers",
+        tickfont=dict(size=20),
+        titlefont=dict(size=25)),
+    yaxis=dict(title=Stats,
+        tickfont=dict(size=20),
+        titlefont=dict(size=25)),
+    legend=dict(title='Types'),
+    width=1280,  # Adjust the width of the plot
+    height=720,  # Adjust the height of the plot
+    title={
+        'text': "Scatter Plot of Pokemon {Stats} vs. Tiers",
+        'x': 0.5,  # Set title's x position to center
+        'xanchor': 'center',  # Anchor title to the center
+        'font': {'size': 30}
+    },
+    margin=dict(t=100),
+    annotations=[dict(
+        text=f"Spearman Correlation: {spearman_corr * -100:.2f}%",
+        x=0.95, y=0.95,
+        xref="paper", yref="paper",
+        showarrow=False,
+        font=dict(color="black", size=12),
+        bgcolor="#19c37d", opacity=0.8
+    )],
+)
+
+# Show plot
+st.plotly_chart(fig_tiers)
